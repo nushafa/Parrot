168633efe96c2a0debc32a56a9e87bbb40917957
Abiat Nushafa <nushafa1@gmail.com>
2025-02-11 21:09:49 +0600

    Add files via upload

---
+++ Parrot.py
@@ -0,0 +1,21 @@
+class Parrot:
+
+    # class attribute
+    species = "bird"
+
+    # instance attribute
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# instantiate the Parrot class
+blu = Parrot("Blu",10)
+woo = Parrot("Woo",15)
+
+#access the class attributes
+print("Blu is a {}".format(blu.species))
+print("Woo is a {}".format (woo.species))
+
+#access the instance attribute
+print("{} is {} years old".format(blu.name, blu.age))
+print("{} is {} years old".format(woo.name, woo.age))
